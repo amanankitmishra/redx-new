9bf32dc027a9d528cbedf1e5e442d327e68f0317
Vishnu <vishnurajput13102000@gmail.com>
2024-01-16 14:50:17 +0530

    done

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: 'out',
   reactStrictMode: true,
   swcMinify: true,
-  output:"export",
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-
 };
 
 module.exports = nextConfig;
